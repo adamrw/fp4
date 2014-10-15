37399f5e5cc4b93d37a252889db8d668b57c7ff3
Adam Weiner <adamwineguy@gmail.com>
2014-10-15 11:18:13 -0600

    finished

--- main.js
+++ main.js
@@ -32,50 +32,32 @@ $(document).on('ready', function() {
 	}
 pushCurrent();
 
+return words
+	.map(function(str) {return parseFloat(str);})
+	.reduce(function(x,y) { return x+y; });
+
+var longestWord = function(str) {
+	var words = str.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, ' ').split(' ');
+	var lengths = words.map(function(w) { return w.length; });
+	var maxLength = Math.max.apply(Math, lengths);
+  return words[lengths.indexOf(maxLength)];
+};
 
 
+var averageStringNumbers = function(str) {
+	var sum = str
+		.split('')
+		.filter(function(c) { return isNumber(c); })
+		.map(function(c) { return parseInt(c); })
+		.reduce(function(x,y) { return x+y; });
+	var letters = str.replace(/[^a-zA-Z]/g, '');
+  return Math.round(sum/letters.length);
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// return words
-// 	.map(function(str) {return parseFloat(str);})
-// 	.reduce(function(x,y) { return x+y; });
-
-// var longestWord = function(str) {
-// 	var words = str.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, ' ').split(' ');
-// 	var lengths = words.map(function(w) { return w.length; });
-// 	var maxLength = Math.max.apply(Math, lengths);
-//   return words[lengths.indexOf(maxLength)];
-// };
-
-
-// var averageStringNumbers = function(str) {
-// 	var sum = str
-// 		.split('')
-// 		.filter(function(c) { return isNumber(c); })
-// 		.map(function(c) { return parseInt(c); })
-// 		.reduce(function(x,y) { return x+y; });
-// 	var letters = str.replace(/[^a-zA-Z]/g, '');
-//   return Math.round(sum/letters.length);
-// };
-
-// module.exports = {
-// 	addNumbers: addNumbers,
-// 	averageStringNumbers: averageStringNumbers,
-// 	longestWord: longestWord,
-// };
+module.exports = {
+	addNumbers: addNumbers,
+	averageStringNumbers: averageStringNumbers,
+	longestWord: longestWord,
+};
 
 });
